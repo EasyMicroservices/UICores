c289a2b2802cba4e7feeb3bcdc14e37dc87aff78
Ali Yousefi <ali.visual.studio@gmail.com>
2023-11-11 20:44:32 +0330

    First initialize

---
+++ src/CSharp/EasyMicroservices.UI.Core.Mvvm/BaseViewModel.cs
@@ -0,0 +1,135 @@
+﻿using EasyMicroservices.ServiceContracts;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.UI.Core;
+
+/// <summary>
+/// 
+/// </summary>
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    bool _IsBusy;
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool IsBusy
+    {
+        get => _IsBusy;
+        set
+        {
+            _IsBusy = value;
+            OnPropertyChanged(nameof(IsBusy));
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="propertyName"></param>
+    public virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(propertyName, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Busy()
+    {
+        IsBusy = true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void UnBusy()
+    {
+        IsBusy = false;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="getServerResult"></param>
+    /// <param name="onSuccess"></param>
+    /// <param name="onError"></param>
+    /// <returns></returns>
+    public async virtual Task FetchData<TResult>(Func<Task<TResult>> getServerResult, Func<TResult, Task> onSuccess, Func<Exception, Task> onError = default)
+    {
+        try
+        {
+            Busy();
+            var result = await getServerResult();
+            var response = result.ToContract<TResult>();
+
+            if (response.IsSuccess)
+                await onSuccess(response);
+            else
+                await DisplayFetchError(response.Error);
+        }
+        catch (Exception ex)
+        {
+            if (onError != null)
+                await onError(ex);
+            else
+                await DisplayError(ex.ToString());
+        }
+        finally
+        {
+            UnBusy();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="getServerResult"></param>
+    /// <param name="onSuccess"></param>
+    /// <param name="onError"></param>
+    /// <returns></returns>
+    public Task FetchData(Func<Task<ErrorContract>> getServerResult, Func<Task> onSuccess, Func<Exception, Task> onError = default)
+    {
+        return FetchData(getServerResult, async (x) =>
+        {
+            await onSuccess();
+        }, onError);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="exception"></param>
+    /// <returns></returns>
+    public virtual Task DisplayError(Exception exception)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="errorContract"></param>
+    /// <returns></returns>
+    public virtual Task DisplayFetchError(ErrorContract errorContract)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public virtual Task DisplayError(string message)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ src/CSharp/EasyMicroservices.UI.Core.Mvvm/Commands/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EasyMicroservices.UI.Core.Commands;
+
+/// <summary>
+/// 
+/// </summary>
+public class RelayCommand : BaseCommand
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="execute"></param>
+    /// <param name="canExecute"></param>
+    public RelayCommand(Action execute, Func<bool> canExecute)
+        : base((x) => execute(), (x) => canExecute())
+    {
+
+    }
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class RelayCommand<T> : BaseCommand
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="execute"></param>
+    /// <param name="canExecute"></param>
+    public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        : base((x) => execute((T)x), (x) => canExecute((T)x))
+    {
+
+    }
+}
