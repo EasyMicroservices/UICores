2ca37edd16a2ff8a6bdff31f40c45df69f1b2332
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-14 12:21:00 +0330

    Support for multiple language UI application

--- src/CSharp/Cores/EasyMicroservices.UI.Cores.Mvvm/BaseViewModel.cs
+++ src/CSharp/Cores/EasyMicroservices.UI.Cores.Mvvm/BaseViewModel.cs
@@ -1,7 +1,11 @@
-﻿using EasyMicroservices.ServiceContracts;
+﻿using EasyMicroservices.Domain.Contracts.Common;
+using EasyMicroservices.ServiceContracts;
 using EasyMicroservices.UI.Cores.Interfaces;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EasyMicroservices.UI.Cores;
@@ -17,6 +21,61 @@ public abstract class BaseViewModel : IBusyViewModel, INotifyPropertyChanged, ID
     public BaseViewModel()
     {
         PropertyChanged += BaseViewModel_PropertyChanged;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public static string CurrentApplicationLanguage { get; set; } = "fa-IR";
+    static ConcurrentDictionary<string, List<LanguageContract>> Languages { get; set; } = new ConcurrentDictionary<string, List<LanguageContract>>();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="languages"></param>
+    public static void AppendLanguage(string key, params LanguageContract[] languages)
+    {
+        if (string.IsNullOrEmpty(key))
+            throw new Exception($"Key cannot be empty!");
+        if (languages == null || languages.Length == 0)
+            throw new Exception($"languages cannot be empty!");
+        if (languages.Any(x => string.IsNullOrEmpty(x.ShortName)))
+            throw new Exception($"language ShortName null or empty detected!");
+
+        if (Languages.TryGetValue(key, out List<LanguageContract> items))
+        {
+            items.AddRange(languages.Where(x => !items.Any(i => i.ShortName == x.ShortName)));
+        }
+        else
+        {
+            Languages[key] = new List<LanguageContract>(languages);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="languageShortName"></param>
+    /// <returns></returns>
+    public string GetLanguage(string key, string languageShortName)
+    {
+        if (Languages.TryGetValue(key, out List<LanguageContract> items))
+        {
+            var find = items.FirstOrDefault(x => x.ShortName.Equals(languageShortName, StringComparison.OrdinalIgnoreCase));
+            if (find != null)
+                return find.Value;
+        }
+        return $"Key {key} not found!";
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    public string GetLanguage(string key)
+    {
+        return GetLanguage(key, CurrentApplicationLanguage);
     }
 
     /// <summary>
