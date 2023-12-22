ad20a1763225a6a749c20a4a4dd6dfd1a0b880e1
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-22 14:57:54 +0330

    add Support for EasyMicroservices.UI.MauiComponents

---
+++ src/CSharp/MauiComponents/EasyMicroservices.UI.MauiComponents/Design/Pages/EasyContentPage.cs
@@ -0,0 +1,30 @@
+using EasyMicroservices.UI.Cores.Interfaces;
+
+namespace EasyMicroservices.UI.MauiComponents.Design.Pages;
+
+public class EasyContentPage : ContentPage, IPage
+{
+    public EasyContentPage()
+    {
+        Loaded += CustomContentPage_Loaded;
+    }
+
+    private void CustomContentPage_Loaded(object sender, EventArgs e)
+    {
+        OnLoadComplete?.Invoke();
+    }
+
+    public Action OnBackBottonPresssed { get; set; }
+    public Action OnLoadComplete { get; set; }
+
+    protected override bool OnBackButtonPressed()
+    {
+        OnBackBottonPresssed?.Invoke();
+        return base.OnBackButtonPressed();
+    }
+
+    public async Task<string> DisplayPrompt(string title, string message)
+    {
+        return await DisplayPromptAsync(title, message);
+    }
+}
