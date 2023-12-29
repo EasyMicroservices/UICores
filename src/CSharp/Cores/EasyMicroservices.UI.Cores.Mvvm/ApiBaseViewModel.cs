d8eb058d884b23ed40d114780be448dcafa103df
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-29 18:57:22 +0330

    add more control on execute api

--- src/CSharp/Cores/EasyMicroservices.UI.Cores.Mvvm/ApiBaseViewModel.cs
+++ src/CSharp/Cores/EasyMicroservices.UI.Cores.Mvvm/ApiBaseViewModel.cs
@@ -13,11 +13,83 @@ public class ApiBaseViewModel : BaseViewModel
     /// 
     /// </summary>
     /// <typeparam name="TResult"></typeparam>
+    /// <typeparam name="TMappedResult"></typeparam>
+    /// <param name="getServerResult"></param>
+    /// <param name="mapResult"></param>
+    /// <param name="onSuccess"></param>
+    /// <param name="onError"></param>
+    /// <returns></returns>
+    public async virtual Task ExecuteMapApi<TResult, TMappedResult>(Func<Task<TResult>> getServerResult, Func<Task<TMappedResult>> mapResult, Func<MessageContract<TMappedResult>, Task> onSuccess, Func<ErrorContract, Task> onError = default)
+    {
+        try
+        {
+            Busy();
+            var result = await getServerResult();
+
+            var response = result.ToContract<TMappedResult>();
+
+            if (response.IsSuccess)
+                await onSuccess(response);
+            else
+                await OnServerErrorHandling(response.Error);
+        }
+        catch (Exception ex)
+        {
+            if (onError != null)
+                await onError(ex);
+            else
+                await OnError(ex);
+        }
+        finally
+        {
+            UnBusy();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
     /// <param name="getServerResult"></param>
     /// <param name="onSuccess"></param>
     /// <param name="onError"></param>
     /// <returns></returns>
-    public async virtual Task ExecuteApi<TResult>(Func<Task<TResult>> getServerResult, Func<MessageContract<TResult>, Task> onSuccess, Func<ErrorContract, Task> onError = default)
+    public async virtual Task ExecuteMapApi<TResult>(Func<Task<TResult>> getServerResult, Func<MessageContract<TResult>, Task> onSuccess, Func<ErrorContract, Task> onError = default)
+    {
+        try
+        {
+            Busy();
+            var result = await getServerResult();
+
+            var response = result.ToContract<TResult>();
+
+            if (response.IsSuccess)
+                await onSuccess(response);
+            else
+                await OnServerErrorHandling(response.Error);
+        }
+        catch (Exception ex)
+        {
+            if (onError != null)
+                await onError(ex);
+            else
+                await OnError(ex);
+        }
+        finally
+        {
+            UnBusy();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    /// <param name="getServerResult"></param>
+    /// <param name="onSuccess"></param>
+    /// <param name="onError"></param>
+    /// <returns></returns>
+    public async virtual Task ExecuteApi<TResult>(Func<Task<object>> getServerResult, Func<MessageContract<TResult>, Task> onSuccess, Func<ErrorContract, Task> onError = default)
     {
         try
         {
